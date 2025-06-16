eabd8822c54343d83c2458df113ed62a7df83c32
Leeveo <101552154+leeveo@users.noreply.github.com>
2025-06-16 17:27:12 +0200

    update

--- app/photobooth-ia/admin/logout/page.tsx
+++ app/photobooth-ia/admin/logout/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useEffect } from 'react';
-import { supabase } from '@/lib/supabaseClient';
+import { createSupabaseClient } from '/lib/supabaseClient';
 import { useRouter } from 'next/navigation';
 
 export default function LogoutPage() {
